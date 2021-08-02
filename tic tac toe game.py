65c1cfba95ea4d32520744e8bf16052ec9221c49
DevProjekts <88111594+DevProjekts@users.noreply.github.com>
2021-08-02 13:15:09 +0200

    Update tic tac toe game.py

--- tic tac toe game.py
+++ tic tac toe game.py
@@ -1,13 +1,18 @@
 from tkinter import *
 import numpy as np
+
 size_of_board = 600
-symbols_size = (size_of_board / 3 - size_of_board / 8) / 2
-symbols_thickness = 50
+symbol_size = (size_of_board / 3 - size_of_board / 8) / 2
+symbol_thickness = 50
 symbol_X_color = '#EE4035'
 symbol_O_color = '#0492CF'
 Green_color = '#7BC043'
 
+
 class Tic_Tac_Toe():
+    # ------------------------------------------------------------------
+    # Initialization Functions:
+    # ------------------------------------------------------------------
     def __init__(self):
         self.window = Tk()
         self.window.title('Tic-Tac-Toe')
@@ -27,6 +32,10 @@ class Tic_Tac_Toe():
         self.X_wins = False
         self.O_wins = False
 
+        self.X_score = 0
+        self.O_score = 0
+        self.tie_score = 0
+
     def mainloop(self):
         self.window.mainloop()
 
@@ -36,29 +45,38 @@ class Tic_Tac_Toe():
 
         for i in range(2):
             self.canvas.create_line(0, (i + 1) * size_of_board / 3, size_of_board, (i + 1) * size_of_board / 3)
+
     def play_again(self):
         self.initialize_board()
         self.player_X_starts = not self.player_X_starts
         self.player_X_turns = self.player_X_starts
         self.board_status = np.zeros(shape=(3, 3))
 
-    def draw_0(self, logical_positions):
-        logical_positions = np.array(logical_positions)
-        grid_positions = self.convert_logical_to_grid_positions(logical_positions)
+    # ------------------------------------------------------------------
+    # Drawing Functions:
+    # The modules required to draw required game based object on canvas
+    # ------------------------------------------------------------------
+
+    def draw_O(self, logical_position):
+        logical_position = np.array(logical_position)
+        # logical_position = grid value on the board
+        # grid_position = actual pixel values of the center of the grid
+        grid_position = self.convert_logical_to_grid_position(logical_position)
         self.canvas.create_oval(grid_position[0] - symbol_size, grid_position[1] - symbol_size,
                                 grid_position[0] + symbol_size, grid_position[1] + symbol_size, width=symbol_thickness,
                                 outline=symbol_O_color)
 
-     def draw_X(self, logical_position):
-            grid_position = self.convert_logical_to_grid_position(logical_position)
+    def draw_X(self, logical_position):
+        grid_position = self.convert_logical_to_grid_position(logical_position)
         self.canvas.create_line(grid_position[0] - symbol_size, grid_position[1] - symbol_size,
                                 grid_position[0] + symbol_size, grid_position[1] + symbol_size, width=symbol_thickness,
                                 fill=symbol_X_color)
         self.canvas.create_line(grid_position[0] - symbol_size, grid_position[1] + symbol_size,
                                 grid_position[0] + symbol_size, grid_position[1] - symbol_size, width=symbol_thickness,
                                 fill=symbol_X_color)
+
     def display_gameover(self):
-    
+
         if self.X_wins:
             self.X_score += 1
             text = 'Winner: Player 1 (X)'
@@ -90,7 +108,11 @@ class Tic_Tac_Toe():
         self.canvas.create_text(size_of_board / 2, 15 * size_of_board / 16, font="cmr 20 bold", fill="gray",
                                 text=score_text)
 
-   
+    # ------------------------------------------------------------------
+    # Logical Functions:
+    # The modules required to carry out game logic
+    # ------------------------------------------------------------------
+
     def convert_logical_to_grid_position(self, logical_position):
         logical_position = np.array(logical_position, dtype=int)
         return (size_of_board / 3) * logical_position + size_of_board / 6
@@ -109,14 +131,14 @@ class Tic_Tac_Toe():
 
         player = -1 if player == 'X' else 1
 
-        
+        # Three in a row
         for i in range(3):
             if self.board_status[i][0] == self.board_status[i][1] == self.board_status[i][2] == player:
                 return True
             if self.board_status[0][i] == self.board_status[1][i] == self.board_status[2][i] == player:
                 return True
 
-        
+        # Diagonals
         if self.board_status[0][0] == self.board_status[1][1] == self.board_status[2][2] == player:
             return True
 
@@ -135,7 +157,7 @@ class Tic_Tac_Toe():
         return tie
 
     def is_gameover(self):
-        
+        # Either someone wins or all grid occupied
         self.X_wins = self.is_winner('X')
         if not self.X_wins:
             self.O_wins = self.is_winner('O')
@@ -174,11 +196,11 @@ class Tic_Tac_Toe():
                     self.board_status[logical_position[0]][logical_position[1]] = 1
                     self.player_X_turns = not self.player_X_turns
 
-            
+            # Check if game is concluded
             if self.is_gameover():
                 self.display_gameover()
-                
-        else:  
+                # print('Done')
+        else:  # Play Again
             self.canvas.delete("all")
             self.play_again()
             self.reset_board = False
